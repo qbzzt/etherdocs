e006173cc269b90206990c88fe22693873684d28
qbzzt <qbzzt1@gmail.com>
2020-05-06 14:56:52 -0500

    Update counter.js

--- startingout/counter.js
+++ startingout/counter.js
@@ -11,17 +11,6 @@ contract("Counter", async accounts => {
                 assert.equal(retVal, 1, "The first increment didn't return one");
         });   // it should return one after incrementing once
 
-
-        it('should return two after incrementing twice', async () => {
-                var counter = await Counter.new();
-
-                await counter.increment();
-                await counter.increment();
-
-                retVal = (await counter.getPastEvents())[0].returnValues[0];
-                assert.equal(retVal, 2, "The two increments didn't return two");
-        });   // it should return two after incrementing twice
-
         it('should return n after incrementing n times', async () => {
                 var counter = await Counter.new();
                 var arr = [];
